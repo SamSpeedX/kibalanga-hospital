6e00dd519570ebe5673af54f191f87fd807ae06f
basanzietech <167584894+basanzietech@users.noreply.github.com>
2024-06-23 21:01:02 +0300

    Create dashboard.php

---
+++ tajiri/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+<body>
+    <h2>Karibu, <?php echo $_SESSION['admin']; ?>!</h2>
+    <a href="logout.php">Exit</a>
+</body>
+</html>
