6984eb2e862064407c95ae0299885eacc05d1f57
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-06-23 21:01:29 +0300

    Update index.php

--- tajiri/index.php
+++ tajiri/index.php
@@ -1,14 +1,17 @@
 <?php
+session_start();
+require_once '../name.php';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Kibalanga Hospital</title>
+    <title><?php echo $system; ?> Hospital</title>
     <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
 </head>
 <body>
     <!-- vizuri nimeona kazi yako kijana-->
-    <h1>Karibu Kibalanga Hospitali</h1>
-    <a href="signin.php">Admin Login</a>
+    <h1>Karibu <?php echo $system; ?> Hospitali</h1>
+    <a href="signin.php">Login as Admin</a>
+    <div onclick="window.location.href='../regitration/signin.php'"></div>
 </body>
 </html>
