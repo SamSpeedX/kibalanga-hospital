fa94f72a6d5a36843bd123d33680a05281d25885
basanzietech <167584894+basanzietech@users.noreply.github.com>
2024-06-23 20:38:23 +0300

    Create index.php

---
+++ tajiri/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kibalanga Hospital</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+    <h1>Karibu Kibalanga Hospitali</h1>
+    <a href="admin/login.php">Admin Login</a>
+</body>
+</html>
