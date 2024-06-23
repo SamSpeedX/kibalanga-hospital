2198c23df439e537252a03b8a5fa58eee96dde8c
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-06-23 20:40:53 +0300

    Update index.php

--- tajiri/index.php
+++ tajiri/index.php
@@ -2,9 +2,10 @@
 <html>
 <head>
     <title>Kibalanga Hospital</title>
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
 </head>
 <body>
+    <!-- vizuri nimeona kazi yako kijana-->
     <h1>Karibu Kibalanga Hospitali</h1>
     <a href="signin.php">Admin Login</a>
 </body>
