8af6a8a99bfa9610c815269d8babb5d3f6a50b36
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-06-23 21:06:22 +0300

    Update dashboard.php

--- tajiri/dashboard.php
+++ tajiri/dashboard.php
@@ -1,15 +1,16 @@
 <?php
-
+require_once '../mfumo/tajiridb.php';
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Admin Dashboard</title>
-    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
 </head>
 <body>
-    <h2>Karibu, <?php echo $_SESSION['admin']; ?>!</h2>
+    <h2>Karibu, <?php echo $jina_la_boss; ?>!</h2>
     <a href="logout.php">Exit</a>
+    
 </body>
 </html>
