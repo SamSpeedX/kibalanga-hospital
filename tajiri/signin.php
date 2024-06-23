59d246afa1d471e46bcb77c2c5aab75beae5b154
basanzietech <167584894+basanzietech@users.noreply.github.com>
2024-06-23 20:23:17 +0300

    Update signin.php

--- tajiri/signin.php
+++ tajiri/signin.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Login</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+<body>
+    <h2>Admin Login</h2>
+    <form method="post" action="">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Sigin">
+    </form>
+</body>
+</html>
