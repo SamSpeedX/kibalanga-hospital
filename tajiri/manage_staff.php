054e7515d36ca5115fe08fe98ca4b9a02d8fc569
Benjamini Omary <167584894+basanzietech@users.noreply.github.com>
2024-06-23 23:53:53 +0300

    Update manage_staff.php

--- tajiri/manage_staff.php
+++ tajiri/manage_staff.php
@@ -1,2 +1,37 @@
 <?php
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Manage Staff</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+<body>
+    <h2>Manage Staff</h2>
+    <a href="dashboard.php">Rudi Nyumbani</a><br><br>
+    <table border="1">
+        <tr>
+            <th>Jina</th>
+            <th>Nafasi</th>
+            <th>Idara</th>
+            <th>Barua Pepe</th>
+            <th>Simu</th>
+            <th>Vitendo</th>
+        </tr>
+<!-- hapa utabadili😁-->
+        <?php while($row = $result->fetch_assoc()) { ?>
+        <tr>
+            <td><?php echo $row['name']; ?></td>
+            <td><?php echo $row['position']; ?></td>
+            <td><?php echo $row['department']; ?></td>
+            <td><?php echo $row['email']; ?></td>
+            <td><?php echo $row['phone']; ?></td>
+            <td>
+                <a href="editmfanyakazi.php?id=<?php echo $row['id']; ?>">Edit</a> |
+                <a href="manage_staff.php?delete=<?php echo $row['id']; ?>" onclick="return confirm('Una uhakika unataka kufuta?')">Futa</a>
+            </td>
+        </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
