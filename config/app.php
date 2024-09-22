7ebfe3ac5dc6150cb41f8c08d29c8992604cecf1
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-09-22 15:26:11 +0300

    Add files via upload

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php 
+define('APP_NAME', 'NAME');
+define('NAME2', '');
+define('NAME', 'Name');
+define('YT_VIDEO', 'youtebe url');
+define('OFFICE_NUMBER', '+255 699722149');
+define('OFFICE_EMAIL', 'name@gmail.com');
+define('DEV_NUMBER', '+255 778760701');
+define('ADRESS', 'Kabuku, Tanga, Tanzania.');
+define('DEV', 'Developed by Sam Technology');
+?>
